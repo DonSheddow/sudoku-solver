19b37b417e1f90fae2d9214265e76ec8caee5b19
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2024-04-21 02:42:04 +0200

    Second commit

--- benchmark.jl
+++ benchmark.jl
@@ -1,14 +1,51 @@
-using Statistics
+using Statistics, DataFrames, CSV
 
 function bench(n)
     samples = []
     for _ in 1:n
-        t = @elapsed run(pipeline(`./target/release/sudoku-solver`, stdin="sudokus/hardest.txt"))
+        t = @elapsed run(pipeline(`taskset -c 0 ./target/release/sudoku-solver`, stdin="sudokus/hardest.txt"))
         push!(samples, t)
     end
     return samples
 end
 
-samples = bench(parse(Int64, ARGS[1]))
-println(mean(samples))
-println(std(samples))
+function compile(flags)
+    run(addenv(`cargo build -r`, "RUSTFLAGS" => flags))
+end
+
+n = parse(Int64, ARGS[1])
+
+# Compile-time options: fat LTO, codegen-units=1, target-cpu=native
+
+FAT_LTO = "-C lto=fat -C embed-bitcode=yes"
+CODEGEN_UNITS = "-C codegen-units=1"
+CPU_NATIVE = "-C target-cpu=native"
+
+samples = DataFrame((time=[], LTO=[], CGU=[], CPU_NATIVE=[]))
+
+for a in ["", FAT_LTO]
+    for b in ["", CODEGEN_UNITS]
+        for c in ["", CPU_NATIVE]
+            settings = (LTO = Int(a != ""),
+                CGU = Int(b != ""),
+                CPU_NATIVE = Int(c != ""))
+            flags = join([a, b, c], " ")
+            println("compiling with $flags")
+            compile(flags)
+            times = bench(n)
+            println("mean: ", mean(times[2:end]))
+            println("stddev: ", std(times[2:end]))
+
+            # skip first sample
+            for t in times[2:end]
+                push!(samples, merge((time=t,), settings))
+            end
+
+            #println(sample)
+        end
+    end
+end
+
+println("----")
+println(samples)
+CSV.write("samples.csv", samples)
